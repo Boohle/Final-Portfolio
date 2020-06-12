561fbcc4d0469d7be79e30a7e6a3feb240df3a92
sicel <sicelubuhlen@gmail.com>
2020-06-12 15:59:43 +0200

    Final work 12/06/2020

---
+++ js/sandbox.js
@@ -0,0 +1,23 @@
+const open = document.querySelector('.mobile-menu');
+const close = document.querySelector('.close');
+const show = document.querySelector('#Nav');
+const mustGo = document.querySelector('.landing');
+
+show.style.display = 'none';
+
+open.addEventListener('click', () =>{
+  show.style.display = 'block';
+  mustGo.style.display = 'none';
+  open.style.display = 'none';
+  close.style.display = 'block';
+  document.body.style.backgroundColor = ' rgb(57, 68, 80)';
+});
+
+close.addEventListener('click', ()=>{
+    mustGo.style.display = 'block';
+    show.style.display = 'none';
+    open.style.display = 'block';
+    close.style.display = 'none';
+    document.body.style.backgroundColor = 'black';
+});
+
